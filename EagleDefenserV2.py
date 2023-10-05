f331ea088ee77cb582d4765c37229e66e6425cec
JimF04 <133442205+JimF04@users.noreply.github.com>
2023-10-04 23:50:50 -0600

    Se actualizó el registro de usuario

--- EagleDefenserV2.py
+++ EagleDefenserV2.py
@@ -1,5 +1,6 @@
 import customtkinter
 import pygame
+import sqlite3
 
 class main_Screen(customtkinter.CTk):
     def __init__(self):
@@ -57,7 +58,21 @@ class LogIn_Screen(customtkinter.CTk):
         app.mainloop()
 
     def login(self):
-        print("Login")
+        connection = sqlite3.connect("users.db")
+        cursor = connection.cursor()
+
+        cursor.execute('''
+        SELECT * FROM users WHERE nickname=? AND password=?
+        ''', (self.entry_username.get(), self.entry_password.get()))
+
+        user = cursor.fetchone()
+        connection.close()
+
+        if user:
+            print("Inicio de sesión exitoso")
+            self.play()  # puedes redirigir al juego después de iniciar sesión con éxito
+        else:
+            print("Error en las credenciales")
 
     def back(self):
         self.destroy()
@@ -69,7 +84,7 @@ class register_Screen(customtkinter.CTk):
         super().__init__()
         self.geometry("800x600")
 
-                # Label de Registro
+        # Label de Registro
         self.label_register = customtkinter.CTkLabel(self, text="Registro")
         self.label_register.place(relx=0.5, rely=0.2, anchor="center")
 
@@ -111,27 +126,49 @@ class register_Screen(customtkinter.CTk):
 
         # Foto
         self.label_foto = customtkinter.CTkLabel(self, text="Foto (ruta): ")
-        self.label_foto.place(relx=0.4, rely=0.9, anchor="center")
+        self.label_foto.place(relx=0.2, rely=0.9, anchor="center")
         # Arrastar y subir foto
         self.entry_foto = customtkinter.CTkEntry(self) 
-        self.entry_foto.place(relx=0.6, rely=0.9, anchor="center")
+        self.entry_foto.place(relx=0.4, rely=0.9, anchor="center")
 
         # Canción Favorita
         self.label_cancion = customtkinter.CTkLabel(self, text="Canción Favorita: ")
-        self.label_cancion.place(relx=0.4, rely=0.10, anchor="center")
+        self.label_cancion.place(relx=0.6, rely=0.9, anchor="center")
         self.entry_cancion = customtkinter.CTkEntry(self)
-        self.entry_cancion.place(relx=0.6, rely=0.10, anchor="center")
+        self.entry_cancion.place(relx=0.8, rely=0.9, anchor="center")
 
         # Botón Registrarse
         self.button_register = customtkinter.CTkButton(self, text="Registrarse", command=self.register)
-        self.button_register.place(relx=0.4, rely=0.11, anchor="center")
+        self.button_register.place(relx=0.4, rely=0.10, anchor="center")
 
         # Botón Volver
         self.button_back = customtkinter.CTkButton(self, text="Volver", command=self.back)
-        self.button_back.place(relx=0.6, rely=0.11, anchor="center")
+        self.button_back.place(relx=0.6, rely=0.10, anchor="center")
     
     def register(self):
-        print("Se registró")
+        connection = sqlite3.connect("users.db")
+        cursor = connection.cursor()
+
+        try:
+            cursor.execute('''
+            INSERT INTO users (nombre, nickname, password, correo, edad, red_social, foto, cancion_favorita)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            ''', (
+                self.entry_nombre.get(),
+                self.entry_nickname.get(),
+                self.entry_password.get(),
+                self.entry_correo.get(),
+                int(self.entry_edad.get()),  # convertir a entero
+                self.entry_red_social.get(),
+                self.entry_foto.get(),
+                self.entry_cancion.get()
+            ))
+            connection.commit()
+            print("Usuario registrado con éxito")
+        except sqlite3.IntegrityError:
+            print("El nickname ya está en uso")
+
+        connection.close()
     
     def back(self):
         self.destroy()
@@ -162,6 +199,29 @@ def start_game():
         game_Screen.fill((0, 0, 0))
         pygame.display.flip()
 
+def setup_database():
+    # Conectar a la base de datos
+    connection = sqlite3.connect("users.db")
+    cursor = connection.cursor()
+    
+    # Crear tabla si no existe
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        nombre TEXT NOT NULL,
+        nickname TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL,
+        correo TEXT NOT NULL,
+        edad INTEGER,
+        red_social TEXT,
+        foto TEXT,
+        cancion_favorita TEXT
+    )
+    ''')
+    
+    connection.commit()
+    connection.close()
+setup_database()
 
 app = main_Screen()
 app.title("Eagle Defender")
